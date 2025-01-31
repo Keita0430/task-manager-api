d94d0fd53695be934d905b1c3cbcaf48b0522788
keita watanabe <k.watanabe@everyleaf.com>
2025-01-31 23:36:03 +0900

    コントローラスペックからリクエストスペックへ変更

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -1,43 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::TasksController, type: :controller do
+RSpec.describe Api::V1::TasksController, type: :request do
   describe 'GET /api/v1/tasks' do
     let!(:tasks) { create_list(:task, 3) }
 
     it 'タスクのリストを返す' do
-      get :index
+      get api_v1_tasks_path
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)['tasks'].size).to eq(3)
     end
   end
 
   describe 'POST /api/v1/tasks' do
-    let(:valid_params) { { task: { title: '数学Ⅱ・Bの宿題', description: '三角関数', status: :todo, position: 1 } } }
-    let(:invalid_params) { { task: { title: '', description: '三角関数', status: 'invalid_status', position: 1 } } }
+    subject { post api_v1_tasks_path, params: params }
 
     context 'パラメータ有効な場合' do
+      let(:params) { { task: { title: '数学Ⅱ・Bの宿題', description: '三角関数', status: :todo, position: 1 } } }
+
       it 'タスクを作成できる' do
-        expect { post :create, params: valid_params }.to change(Task, :count).by(1)
+        expect { subject }.to change(Task, :count).by(1)
       end
 
       it '201を返す' do
-        post :create, params: valid_params
+        subject
         expect(response).to have_http_status(:created)
       end
     end
 
     context 'パラメータが無効な場合' do
+      let(:params) { { task: { title: '', description: '三角関数', status: 'invalid_status', position: 1 } } }
+
       it 'タスクを作成できない' do
-        expect { post :create, params: invalid_params }.to_not change(Task, :count)
+        expect { subject }.to_not change(Task, :count)
       end
 
       it '422を返す' do
-        post :create, params: invalid_params
+        subject
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'エラーを返す' do
-        post :create, params: invalid_params
+        subject
         expect(JSON.parse(response.body)['errors']).to be_present
       end
     end
@@ -45,12 +48,13 @@ RSpec.describe Api::V1::TasksController, type: :controller do
 
   describe 'PATCH /api/v1/tasks/:id' do
     let!(:task) { create(:task, title: 'Old Title', description: 'Old Description', status: :todo, position: 1) }
-    let(:valid_params) { { task: { title: 'New Title', description: 'New Description', status: :done, position: 1 } } }
-    let(:invalid_params) { { task: { title: '', description: 'New Description', status: :done, position: 1 } } }
+    subject { patch api_v1_task_path(task), params: params }
 
     context 'パラメータ有効な場合' do
+      let(:params) { { task: { title: 'New Title', description: 'New Description', status: :done, position: 1 } } }
+
       it 'タスクを更新できる' do
-        patch :update, params: valid_params.merge(id: task.id)
+        subject
         task.reload
         expect(task.title).to eq('New Title')
         expect(task.description).to eq('New Description')
@@ -59,26 +63,28 @@ RSpec.describe Api::V1::TasksController, type: :controller do
       end
 
       it '200を返す' do
-        patch :update, params: valid_params.merge(id: task.id)
+        subject
         expect(response).to have_http_status(:ok)
       end
     end
 
     context 'パラメータが無効な場合' do
+      let(:params) { { task: { title: '', description: 'New Description', status: :done, position: 1 } } }
+
       it 'タスクを更新できない' do
         original_title = task.title
-        patch :update, params: invalid_params.merge(id: task.id)
+        subject
         task.reload
         expect(task.title).to eq(original_title)
       end
 
       it '422を返す' do
-        patch :update, params: invalid_params.merge(id: task.id)
+        subject
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'エラーを返す' do
-        patch :update, params: invalid_params.merge(id: task.id)
+        subject
         expect(JSON.parse(response.body)['errors']).to be_present
       end
     end
@@ -88,14 +94,15 @@ RSpec.describe Api::V1::TasksController, type: :controller do
     let!(:task1) { Task.create!(title: 'Task 1', status: :todo, position: 1) }
     let!(:task2) { Task.create!(title: 'Task 2', status: :todo, position: 2) }
     let!(:task3) { Task.create!(title: 'Task 3', status: :todo, position: 3) }
+    subject { delete api_v1_task_path(task1) }
 
     context 'タスクが存在する場合' do
       it 'タスクを削除できる' do
-        expect { delete :destroy, params: { id: task1.id } }.to change(Task, :count).by(-1)
+        expect { subject }.to change(Task, :count).by(-1)
       end
 
       it '位置が更新される' do
-        delete :destroy, params: { id: task1.id }
+        subject
         task2.reload
         task3.reload
         expect(task2.position).to eq(1)
@@ -103,12 +110,12 @@ RSpec.describe Api::V1::TasksController, type: :controller do
       end
 
       it '200を返す' do
-        delete :destroy, params: { id: task1.id }
+        subject
         expect(response).to have_http_status(:ok)
       end
 
       it 'タスクのリストを返す' do
-        delete :destroy, params: { id: task1.id }
+        subject
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)['tasks'].size).to eq(2)
       end
@@ -116,7 +123,7 @@ RSpec.describe Api::V1::TasksController, type: :controller do
 
     context 'タスクが存在しない場合' do
       it '404を返す' do
-        delete :destroy, params: { id: 0 }
+        delete api_v1_task_path(100)
         expect(response).to have_http_status(:not_found)
       end
     end
@@ -126,12 +133,13 @@ RSpec.describe Api::V1::TasksController, type: :controller do
     let!(:task1) { create(:task, status: :todo, position: 1) }
     let!(:task2) { create(:task, status: :todo, position: 2) }
     let!(:task3) { create(:task, status: :todo, position: 3) }
-    let(:valid_params) { { task: { id: task1.id, status: 'done', position: 1 } } }
-    let(:invalid_params) { { task: { id: task1.id, status: 'invalid_status', position: 2 } } }
+    subject { post reorder_api_v1_tasks_path, params: params }
 
     context 'パラメータが有効な場合' do
+      let(:params) { { task: { id: task1.id, status: 'done', position: 1 } } }
+
       it 'ステータスと位置が更新される' do
-        post :reorder, params: valid_params
+        subject
         task1.reload
         task2.reload
         task3.reload
@@ -142,32 +150,34 @@ RSpec.describe Api::V1::TasksController, type: :controller do
       end
 
       it '200を返す' do
-        post :reorder, params: valid_params
+        subject
         expect(response).to have_http_status(:ok)
       end
 
       it 'タスクのリストを返す' do
-        get :reorder, params: valid_params
+        subject
         expect(response).to have_http_status(:ok)
         expect(JSON.parse(response.body)['tasks'].size).to eq(3)
       end
     end
 
     context 'パラメータが無効な場合' do
+      let(:params) { { task: { id: task1.id, status: 'invalid_status', position: 2 } } }
+
       it 'タスクのステータスを更新できない' do
         original_status = task1.status
-        post :reorder, params: invalid_params
+        subject
         task1.reload
         expect(task1.status).to eq(original_status)
       end
 
       it '422を返す' do
-        post :reorder, params: invalid_params
+        subject
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'エラーを返す' do
-        post :reorder, params: invalid_params
+        subject
         expect(JSON.parse(response.body)['errors']).to be_present
       end
     end
